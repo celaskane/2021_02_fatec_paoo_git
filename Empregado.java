51f0519d63cdd718667a39db70dceede6cca0b5e
Wired <leandrou9@gmail.com>
2021-08-04 14:11:08 -0300

    Cálculo de salário efetuado de acordo com o tipo

---
+++ Empregado.java
@@ -0,0 +1,36 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario(){
+        if(tipo == 1){
+            return salario;
+        }
+        else if(tipo == 2){
+            return salario +salario * comissao;
+        }
+        else if(tipo == 3){
+            return salario + bonus;
+        }
+        return 0;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setName(String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+}
